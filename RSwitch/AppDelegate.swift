e1a136d50ec2e544359a04b5b94e482ac094eb62
hrbrmstr <bob@rud.is>
2019-08-22 01:44:54 -0400

    Initial Commit

---
+++ RSwitch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RSwitch
+//
+//  Created by hrbrmstr on 8/22/19.
+//  Copyright © 2019 Bob Rudis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ RSwitch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSwitch
+//
+//  Created by hrbrmstr on 8/22/19.
+//  Copyright © 2019 Bob Rudis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
